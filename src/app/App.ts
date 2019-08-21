4600e823daec695bee669e91c88a3221efa6afa4
maxfahl <max@pixilab.se>
2019-08-21 11:54:07 +0200

    Initial commit

--- src/app/App.ts
+++ src/app/App.ts
@@ -2,11 +2,27 @@
 
 export class App {
 
+	private mContainer: HTMLDivElement;
+	private mButtons: HTMLDivElement[];
+
 	constructor() {
 		this.init();
 	}
 
 	private init(): void {
-		
+		this.mContainer = document.querySelector('#wrapper .inner');
+		this.mButtons = Array.prototype.slice.call(
+			this.mContainer.querySelectorAll<HTMLDivElement>('.tabs .buttons .button')
+		);
+		console.log(this.mButtons);
+		this.mButtons.forEach(buttonEl => {
+			buttonEl.addEventListener('click', (e) => this.onButtonClick(<HTMLDivElement>e.target));
+		});
+		this.onButtonClick(this.mButtons[0]);
+	}
+
+	private onButtonClick(selectedButton: HTMLDivElement): void {
+		this.mButtons.forEach(buttonEl => buttonEl.classList.remove('active'));
+		selectedButton.classList.add('active');
 	}
 }
