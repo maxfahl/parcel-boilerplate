80a16cd60322c1e9642533216d05c1e276cb4736
maxfahl <max@pixilab.se>
2019-08-23 08:02:53 +0200

    Start chrome for blocks on tab buttons press.

--- src/app/App.ts
+++ src/app/App.ts
@@ -8,6 +8,8 @@ export class App {
 	private mTabButtons: HTMLDivElement[];
 	private mLists: HTMLDivElement[];
 	private mControlPanes: HTMLDivElement[];
+	private mEnablePaneTimeout: number;
+	private mPresentationStepperButtons: HTMLDivElement[];
 	private mMoviesPlayPauseButtons: HTMLDivElement[];
 	private mPubSub: PubSubPeer;
 	private mFileNameToClass: any = {
@@ -29,15 +31,21 @@ export class App {
 		this.mTabButtons = Array.prototype.slice.call(
 			this.mContainer.querySelectorAll<HTMLDivElement>('.tabs .buttons .button')
 		);
+		this.mTabButtons.forEach(buttonEl => {
+			buttonEl.addEventListener('click', (e) => this.onTabButtonClick(<HTMLDivElement>e.target, true));
+		});
 		this.mLists = Array.prototype.slice.call(
 			this.mContainer.querySelectorAll<HTMLDivElement>('.tabs .content .list')
 		);
-		this.mTabButtons.forEach(buttonEl => {
-			buttonEl.addEventListener('click', (e) => this.onTabButtonClick(<HTMLDivElement>e.target));
-		});
 		this.mControlPanes = Array.prototype.slice.call(
 			this.mContainer.querySelectorAll<HTMLDivElement>('.controls .pane')
 		);
+		this.mPresentationStepperButtons = Array.prototype.slice.call(
+			this.mContainer.querySelectorAll<HTMLDivElement>('.controls .pane.presentations .stepper')
+		);
+		this.mPresentationStepperButtons.forEach(button => {
+			button.addEventListener('click', () => this.onPresentationStepButtonPress(button));
+		});
 		this.mMoviesPlayPauseButtons = Array.prototype.slice.call(
 			this.mContainer.querySelectorAll<HTMLDivElement>('.controls .pane.movies .play-pause .button')
 		);
@@ -94,16 +102,26 @@ export class App {
 		);
 	}
 
-	private onTabButtonClick(selectedButton: HTMLDivElement): void {
+	private onTabButtonClick(
+		selectedButton: HTMLDivElement,
+		selectedByUser: boolean = false
+	): void {
 		this.mTabButtons.forEach(buttonEl => buttonEl.classList.remove('active'));
 		this.mLists.forEach(buttonEl => buttonEl.classList.remove('visible'));
-		const list = selectedButton.dataset['type'] === 'presentations' ? this.mLists[0] : this.mLists[1];
+		const wantedType = selectedButton.dataset['type'];
+		const list = wantedType === 'presentations' ? this.mLists[0] : (wantedType === 'movies' ? this.mLists[1] : this.mLists[2]);
 
 		selectedButton.classList.add('active');
 		list.classList.add('visible');
 
 		this.mContainer.querySelectorAll('.controls .pane').forEach(pane => pane.classList.remove('visible'));
-		this.mContainer.querySelector(`.controls .pane.${ selectedButton.dataset['type'] }`).classList.add('visible');
+		this.mContainer.querySelector(`.controls .pane.${ wantedType }`).classList.add('visible');
+
+		if (selectedByUser && wantedType === 'blocks') {
+			if (this.mCurrentFile)
+				this.setCurrentFile('');
+			this.mPubSub.set(`Network.${ this.mTarget }.program`, 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe|C:\\Program Files (x86)\\Google\\Chrome\\Application|--kiosk --disable-features=TranslateUI --app=http://10.0.1.157:9080/spot');
+		}
 	}
 
 	private markCurrentListItem() {
@@ -119,13 +137,25 @@ export class App {
 		let listItemToSelect = className ? this.mContainer.querySelector<HTMLDivElement>(`.tabs .content .${ className }`) : null;
 		if (this.mLastSelectedItem && this.mLastSelectedItem !== listItemToSelect)
 			this.mLastSelectedItem.classList.remove('selected');
+		if (this.mEnablePaneTimeout) {
+			window.clearTimeout(this.mEnablePaneTimeout);
+			this.mEnablePaneTimeout = undefined;
+		}
 		this.mControlPanes.forEach(pane => pane.classList.remove('enabled'));
 		if (listItemToSelect) {
 			listItemToSelect.classList.add('selected');
 			this.mLastSelectedItem = listItemToSelect;
 			this.onTabButtonClick(inPresentations ? this.mTabButtons[0] : this.mTabButtons[1]);
 			const paneToEnable = inPresentations ? this.mControlPanes[0] : this.mControlPanes[1];
-			paneToEnable.classList.add('enabled');
+			if (inPresentations) {
+				paneToEnable.classList.add('enabled');
+			} else {
+				this.mEnablePaneTimeout = window.setTimeout(() => {
+					paneToEnable.classList.add('enabled');
+				}, 2500)
+			}
+		} else {
+			this.onTabButtonClick(this.mTabButtons[2]);
 		}
 	}
 
@@ -161,29 +191,29 @@ export class App {
 		this.mPubSub.set(`Realm.Main.variable.${ this.mTarget }MoviePlaying.value`, false);
 
 		if (this.mCurrentFile === fileName) {
-			this.mPubSub.set(`Network.${ this.mTarget }.currFile`, '');
+			this.setCurrentFile('');
+
 		} else {
-			this.mPubSub.set(`Network.${ this.mTarget }.currFile`, fileName);
+			this.setCurrentFile(fileName);
 			this.resetMoviesPlayPauseButton();
-			console.log('huh? ' + listItem.parentElement.classList.contains('movies'));
 			if (listItem.parentElement.classList.contains('movies'))
 				this.mPubSub.set(`Realm.Main.variable.${ this.mTarget }MoviePlaying.value`, true);
 		}
+	}
+
+	private setCurrentFile(fileName: string): void {
+		this.mPubSub.set(`Network.${ this.mTarget }.currFile`, fileName);
+	}
+
+	private onPresentationStepButtonPress(button: HTMLDivElement) {
+		let next = button.classList.contains('next');
+		this.mPubSub.set(`Network.${ this.mTarget }.keyDown`, next ? 'VK_RIGHT' : 'VK_LEFT');
 	}
 
 	private onMoviesPlayPauseButtonClick(button: HTMLDivElement): void {
 		let doPause = button.classList.contains('pause');
 		this.setMoviePlatingState(doPause);
 		this.mPubSub.set(`Network.${ this.mTarget }.keyDown`, 'control+P');
-
-		// if (doPause) {
-		// 	this.mMoviesPlayPauseButtons[0].classList.add('visible');
-		// 	this.mMoviesPlayPauseButtons[1].classList.remove('visible');
-		// } else {
-		// 	this.mMoviesPlayPauseButtons[0].classList.remove('visible');
-		// 	this.mMoviesPlayPauseButtons[1].classList.add('visible');
-		// }
-		// this.mPubSub.set(`Network.${ this.mTarget }.keyDown`, 'control+P');
 	}
 
 	private setMoviePlatingState(playing: boolean): void {
@@ -191,7 +221,6 @@ export class App {
 	}
 
 	private onMoviePlayStateChange(playing: boolean) {
-		console.log(playing);
 		if (playing) {
 			this.mMoviesPlayPauseButtons[0].classList.remove('visible');
 			this.mMoviesPlayPauseButtons[1].classList.add('visible');
